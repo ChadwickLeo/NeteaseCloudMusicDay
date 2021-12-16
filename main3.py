20f8c82b8e0f4a38f27007e8b7234853b582070d
ChadwickLeo <34240557+ChadwickLeo@users.noreply.github.com>
2021-12-16 21:33:07 +0800

    Update main3.py

--- main3.py
+++ main3.py
@@ -175,6 +175,6 @@ if __name__=='__main__':
                 if res:
                     print(f'━━▶添加歌曲列表成功【{str(len(will_add_list))}】：{music_ids}')
                 else:
-                    print(f'━━▶添加歌曲列表失败【{str(len(will_add_list))}】：{music_ids}')
+                    print(f'◀━━添加歌曲列表失败【{str(len(will_add_list))}】：{music_ids}')
     except:
         print('error')
